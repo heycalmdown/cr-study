f9024a91e9160aba6f2ae10fe4d343a4b4fcdc17
heycalmdown <heyacct@gmail.com>
2019-09-06 17:53:21 +0900

    걸음마

--- src/cr-study.cr
+++ src/cr-study.cr
@@ -1,6 +1,25 @@
+require "http/server"
+def one(x)
+  x + 1
+end
+
+
 # TODO: Write documentation for `Cr::Study`
 module Cr::Study
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  proc = ->one(Int32)
+  puts proc.call 1
+  if 0
+    puts "got ya"
+  end
+
+  # server = HTTP::Server.new do |context|
+  #   context.response.content_type = "text/plain"
+  #   context.response.print "Hey world! The time is #{Time.now}"
+  # end
+
+  # address = server.bind_tcp 8080
+  # puts "Listening on http://#{address}"
+  # server.listen
 end
